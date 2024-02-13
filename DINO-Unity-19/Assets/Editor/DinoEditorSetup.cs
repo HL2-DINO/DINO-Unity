0fdac577d6d02ffaf34a44ce8e9fd3c6ad2ddde3
Hisham <45574098+hiqb217@users.noreply.github.com>
2024-02-13 05:36:54 +0000

    initial commit

---
+++ DINO-Unity-19/Assets/Editor/DinoEditorSetup.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using UnityEditor;
+using ToolTrackingUtils;
+using ToolConfigUtilities;
+using Newtonsoft.Json.Linq;
+
+/** @file           DinoEditorSetup.cs
+ *  @brief          An Editor helper script which helps to properly populate the ToolsTrackedByHololens member of a chosen
+ *                  \p UnityToolManager script that you specify. Generally, this will help setup your app based on data read
+ *                  in from a correctly formatted JSON config object. Should be used as the only interaction point between Unity
+ *                  and the tool config object.
+ *
+ *  @author         Hisham Iqbal
+ *  @copyright      &copy; 2023 Hisham Iqbal
+ */
+
+public class DinoEditorSetup : EditorWindow
+{
+    /// <summary>
+    /// A parent GameObject which will house all of the GameObjects associated with each of our tracked tools
+    /// </summary>
+    Transform TrackedToolsParentTransform;
+
+    /// <summary>
+    /// Full filepath for where the JSON config file is
+    /// </summary>
+    string JSONPath = "";
+
+    /// <summary>
+    /// A UnityToolManager script which should be part of your scene
+    /// </summary>
+    UnityToolManager toolManagerInstance;
+
+    /// <summary>
+    /// The script which communicates with the C++ DLL directly, should be part of your scene
+    /// </summary>
+    ResearchModeController rmControllerInstance;
+
+    /// <summary>
+    /// An internally stashed JObject equivalent of the config object contained in \p JSONPath
+    /// </summary>
+    private JObject ToolConfigJson;
+
+    [MenuItem("DINO Unity/DINO Setup")]
+    public static void Apply()
+    {
+        EditorWindow.GetWindow(typeof(DinoEditorSetup));
+    }
+
+    void LaunchFilePicker()
+    {
+        JSONPath = EditorUtility.OpenFilePanel("Pick tool config json file", Application.dataPath, "json");        
+    }
+
+    /// <summary>
+    /// Creates a bunch of GameObjects based on the tool config file info, and will also create some small spheres to 
+    /// appear at the centre of each tool-marker centre
+    /// </summary>
+    void PopulateObjects()
+    {
+        if (toolManagerInstance == null) { Debug.LogError("No UnityToolManager specified"); return; }
+        if (TrackedToolsParentTransform == null) { Debug.LogError("No parent transform specified"); return; }
+        if (ToolConfigJson == null) { Debug.LogError("ToolConfig JSON was not read in properly"); return; }
+
+        // set the public List after parsing the JSON
+        toolManagerInstance.ToolsTrackedByHololens = JSONUtils.CreateTrackedToolsetFromJSON(ToolConfigJson);
+
+        var toolsList = toolManagerInstance.ToolsTrackedByHololens;
+
+        // delete all the child-objects of TrackedtoolsParentTransform
+        for (int i = TrackedToolsParentTransform.childCount - 1; i > -1; --i)
+        {
+            GameObject.DestroyImmediate(TrackedToolsParentTransform.GetChild(i).gameObject);
+        }
+
+        // populate a list of TrackedTools based on information read in from the config file
+        for (int i = 0; i < toolsList.Count; i++)
+        {
+            var tool = toolsList[i];
+            GameObject toolGameObject = new GameObject(tool.ToolName);
+            toolGameObject.transform.parent = TrackedToolsParentTransform;
+            toolGameObject.transform.localScale = Vector3.one;
+            toolGameObject.transform.localPosition = Vector3.zero;
+            toolGameObject.transform.localRotation = Quaternion.identity;
+            tool.ToolUnityTransform = toolGameObject.transform;
+        }
+
+        // add in the marker centres so we can see them in Unity
+        UnitySceneSetup.AddMarkerCentreSpheres(toolsList);
+    }
+
+
+    void OnGUI()
+    {
+        GUILayout.Label("DINO Settings", EditorStyles.boldLabel);
+
+        GUILayout.Space(10);
+        toolManagerInstance = (UnityToolManager)EditorGUILayout.ObjectField("(1) Pick ToolManager", toolManagerInstance, typeof(UnityToolManager), true);
+
+        GUILayout.Space(10);
+        rmControllerInstance = (ResearchModeController)EditorGUILayout.ObjectField("(2) Pick ResearchModeController", 
+            rmControllerInstance, 
+            typeof(ResearchModeController), true);
+
+        GUILayout.Space(10); // Add some vertical spacing     
+        EditorGUILayout.LabelField("(3) Select JSON file");
+        if (GUILayout.Button("Open file picker"))
+        {
+            LaunchFilePicker();
+            Debug.Log($"JSON path selected: {JSONPath}");
+        }
+
+        GUILayout.Space(10);
+        EditorGUILayout.LabelField("JSON selected filename:");
+        EditorGUILayout.LabelField($"{System.IO.Path.GetFileName(JSONPath)}");
+
+        GUILayout.Space(10); // Add some vertical spacing
+        EditorGUILayout.LabelField("(4) Parent transform for all TrackedTools");
+        TrackedToolsParentTransform = (Transform)EditorGUILayout.ObjectField(TrackedToolsParentTransform, typeof(Transform), true);
+
+        GUILayout.Space(10); // Add some vertical spacing    
+        EditorGUILayout.LabelField("(5) Populate Objects");
+        if (GUILayout.Button("Create Objects & Apply JSON Settings"))
+        {
+            RetrieveJSONProperties();
+            PopulateObjects();
+        }
+    }
+
+    /// <summary>
+    /// Will try to create our internal JObject by reading filepath
+    /// </summary>
+    private void RetrieveJSONProperties()
+    {
+        if (JSONPath == "") { Debug.LogError("No JSON filepath specified"); return; }
+        if (toolManagerInstance == null) { Debug.LogError("No UnityToolManager specified"); return; }
+
+        ToolConfigJson = JSONUtils.TryReadingToolConfigJSON(JSONPath);
+        if (ToolConfigJson == null) { Debug.LogError("TryReadingToolConfigJSON error thrown"); return; }
+
+        if(rmControllerInstance == null) { Debug.LogError("Please assign an instance of ResearchModeController from Dino Unity > Dino Editor Setup"); return; }
+        // if it gets here, then it's passed the checks we placed, so set the filepath 
+        rmControllerInstance.JSONFilename = System.IO.Path.GetFileName(JSONPath);
+
+    }
+}
---
+++ DINO-Unity-19/Assets/Scripts/ToolTrackingUtils.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/** @file           ToolTrackingUtils.cs
+ *  @brief          Defines some app-wide specifics for IR tool tracking and also helper functions
+ *
+ *  @author         Hisham Iqbal
+ *  @copyright      &copy; 2023 Hisham Iqbal
+ */
+
+namespace ToolTrackingUtils
+{
+    /// <summary>
+    /// Struct universally used through the HL-DINO-Unity app to define a 'trackable' tool
+    /// </summary>
+    [System.Serializable]
+    public class TrackedTool
+    {
+        /// <summary>
+        /// Numeric ID which is passed into the HL2-DINO-DLL. This is cast into a uint8 on the C++ side.
+        /// </summary>
+        public int ToolID;
+
+        /// <summary>
+        /// String identifier, purely for labelling in the Unity Editor
+        /// </summary>
+        public string ToolName;
+
+        /// <summary>
+        /// XYZ triplets of marker-centres in metres (left-handed for Unity)
+        /// </summary>
+        public List<Vector3> ToolMarkerTriplets;
+
+        /// <summary>
+        /// Unity transform corresponding to the tool that we will position
+        /// </summary>
+        public Transform ToolUnityTransform;
+
+        /// <summary>
+        /// Transform matrix of tool pose, left-handed and in metres
+        /// </summary>
+        [HideInInspector]
+        public Matrix4x4 Tool_HoloFrame_LH;
+
+        /// <summary>
+        /// Flag bool indicating if the HL2 can see the tool in the last set of sensor frames
+        /// </summary>
+        [HideInInspector]
+        public bool VisibleToHoloLens;
+
+        /// <summary>
+        /// Timestamp updated on the Unity side to track how long a tool has been visible for
+        /// </summary>
+        [HideInInspector]
+        public float TimestampLastSeen;        
+    }
+
+    public static class DinoPluginUtils
+    {
+        /// <summary>
+        /// Function will generate a coded formatted string with tool IDs and triplets
+        /// </summary>
+        /// <returns>Formatted tool-string to be passed into DLL</returns>
+        private static string GenerateToolString(List<TrackedTool> toolsToTrack)
+        {
+            // function will produce a formatted string as follows:
+            // "toolID,x1,y1,z1,..xn,yn,zn;toolID2,...."
+
+            string msg = "";
+
+            foreach (TrackedTool tool in toolsToTrack)
+            {
+                msg += (tool.ToolID).ToString() + ",";
+
+                List<Vector3> triplets = tool.ToolMarkerTriplets;
+
+                for (int i = 0; i < triplets.Count - 1; i++)
+                {
+                    msg += string.Format("{0:F5},{1:F5},{2:F5},", triplets[i].x, triplets[i].y, triplets[i].z);
+                }
+
+                // add semi-colon after final triplet
+                msg += string.Format("{0:F5},{1:F5},{2:F5};", triplets[triplets.Count - 1].x, triplets[triplets.Count - 1].y, triplets[triplets.Count - 1].z);
+
+            }
+
+            msg.TrimEnd(';');
+            return msg;
+        }
+    }
+
+    /// <summary>
+    /// A helper class for parsing Tool Config data
+    /// </summary>
+    public static class UnitySceneSetup
+    {
+        /// <summary>
+        /// Helper function for creating some sphere GameObjects, located at marker centres of a tool. These are attached properly 
+        /// to object transforms so that you can visualise little spheres when wearing the HL2 and looking at a tracked tool through 
+        /// the headset.
+        /// </summary>
+        /// <param name="toolsToAdd">A properly filled list of \p TrackedTool which we will use to instantiate marker objects</param>
+        public static void AddMarkerCentreSpheres(List<TrackedTool> toolsToAdd)
+        {
+            foreach (var tool in toolsToAdd)
+            {
+                if (tool.ToolUnityTransform == null) continue;
+                                
+                // parent for all of our marker centre spheres
+                GameObject markerParent = new GameObject("tracker");
+                
+                markerParent.transform.parent = tool.ToolUnityTransform;
+                markerParent.transform.localPosition = Vector3.zero;
+                markerParent.transform.localRotation = Quaternion.identity;
+                
+                int markerID = 0; // used to label each marker we add
+                foreach (var coordinate in tool.ToolMarkerTriplets)
+                {
+                    // add a new sphere for each tool marker we have
+                    GameObject marker = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    marker.name = "Marker_" + markerID.ToString();
+                    marker.transform.parent = markerParent.transform;
+
+                    float sphereRadiusMetres = 0.005f;
+                    marker.transform.localScale = new Vector3(sphereRadiusMetres, sphereRadiusMetres, sphereRadiusMetres);
+                    marker.transform.localRotation = Quaternion.identity;
+                    marker.transform.localPosition = coordinate;
+
+                    ++markerID;
+                }
+            }
+        }
+    }
+
+}
